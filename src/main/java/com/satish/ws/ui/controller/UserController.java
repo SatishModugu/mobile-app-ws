fedf8bdbffe31d8e3afdc278020bb30e6a412590
Satish Reddy Modugu <32131478+SatishModugu@users.noreply.github.com>
2019-08-12 17:21:43 -0500

    Using Request Parameter and returning java object

--- src/main/java/com/satish/ws/ui/controller/UserController.java
+++ src/main/java/com/satish/ws/ui/controller/UserController.java
@@ -1,30 +1,53 @@
 package com.satish.ws.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //Register class a RestCcontroller able to receive HTTP request
-@RequestMapping("users")//http://localhost8080/users/
-public class UserController {
+import com.satish.ws.ui.model.response.UserRest;
+
+@RestController // Register class a RestCcontroller able to receive HTTP request
+@RequestMapping("/users") // http://localhost8080/users/
+public class UserController
+{
 	@GetMapping
-	public String getUser()
+	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "1") int limit,
+			@RequestParam(value = "sort", required = false) String sort)
 	{
-		return "get user was called";
+		// Not initialized primitive data types can not be translated to null,
+		// so required is mostly used with string types
+		return "get users was called with page =" + page + "limit =" + limit;
 	}
+
+	@GetMapping(path = "/{userId}", produces= {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE} )
+	public UserRest getUser(@PathVariable String userId)
+	{
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail("test@test.com");
+		returnValue.setFirstName("Satish");
+		returnValue.setLastName("modugu");
+		return returnValue;
+	}
+
 	@PostMapping
 	public String createUser()
 	{
 		return "creat user was called";
 	}
+
 	@PutMapping
 	public String updateUser()
 	{
 		return "update user was called";
 	}
+
 	@DeleteMapping
 	public String deleteUser()
 	{
