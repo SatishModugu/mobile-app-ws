885af8f81e667552fa9ef749a57a19260f06c912
smodugu <smodugu@IST242-NUC011.unomaha.edu>
2019-08-19 16:04:11 -0500

    Request Model

---
+++ src/main/java/com/satish/ws/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,50 @@
+package com.satish.ws.ui.model.request;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+public class UserDetailsRequestModel
+{
+	@NotNull(message = "Message")
+	private String firstName;
+	@NotNull(message = "Message")
+	private String lastName;
+	@NotNull(message = "Message")
+	@Email
+	private String email;
+	@NotNull(message = "Message")
+	@Size(min = 8, max = 16, message = "Passward not meeting requirements")
+	private String password;
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	public String getPassword()
+	{
+		return password;
+	}
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+}
